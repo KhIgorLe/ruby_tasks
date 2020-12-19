533d76985ea7d5223391ac263f127fc13d2e97cc
KhIgorLe <igor.kharchuk85@gmail.com>
2020-12-19 18:29:52 +0200

    tasks_complete

--- task_1/truncate.rb
+++ task_1/truncate.rb
@@ -1,2 +1,12 @@
 module Truncate
+  def truncate(*attr_names, length:)
+    attr_names.each do |attr_name|
+      var_name = "@#{attr_name}".to_sym
+
+      define_method("#{attr_name}=".to_sym) do |value|
+        value = "#{value.slice(0, length)}..." if value.size > length
+        instance_variable_set(var_name, value)
+      end
+    end
+  end
 end
--- task_2/array.rb
+++ task_2/array.rb
@@ -1,2 +1,7 @@
 class Array
+  def rebuild!
+    map! do |item|
+      item.is_a?(Array) ? item.map { |i| yield(i, item.size) } : yield(item, size)
+    end
+  end
 end

04af1b3f1b4f683bd93535fda030566151541fbd
KhIgorLe <igor.kharchuk85@gmail.com>
2020-12-20 12:15:06 +0200

    fixed truncate

--- task_1/truncate.rb
+++ task_1/truncate.rb
@@ -4,6 +4,7 @@ module Truncate
       var_name = "@#{attr_name}".to_sym
 
       define_method("#{attr_name}=".to_sym) do |value|
+        value = value.strip
         value = "#{value.slice(0, length)}..." if value.size > length
         instance_variable_set(var_name, value)
       end
